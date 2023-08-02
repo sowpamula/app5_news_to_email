2c6a6acce2b0915173075aa312ef84aa44a4a8d1
sowjanya <sowjanyapamulapati55@gmail.com>
2023-08-02 16:22:22 -0700

    Send an email with news tiles and descriptions

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
 import requests
+from send_email import send_email
 
 api_key = "866b896303d2470fbe461eea7c1db3dd"
 url ="https://newsapi.org/v2/everything?q=tesla&from=2023-07-02&sortBy=publishedAt&apiKey=866b896303d2470fbe461eea7c1db3dd"
 
 # Make request
 request = requests.get(url)
+
 # Get a dictionary with data
 content = request.json()
-# Access the article titles and description
-for article in content["articles"]:
-    print(article["title"])
-    print(article["description"])
 
+# Access the article titles and description
+body = ""
+for article in content["articles"]:
+    if article["title"] is not None:
+        body = body + article["title"] + "\n" + article["description"] + 2*"\n"
+
+body = body.encode("utf-8")
+send_email(message=body)
